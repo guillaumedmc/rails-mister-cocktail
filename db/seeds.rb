f54c8744b3ec1822369e5f6eafebd7f9d0e04785
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-02-14 17:49:31 +0100

    fonctionnalités finies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks = open(url).read
+ingredients = JSON.parse(drinks)
+
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Caïpirinha")
+Cocktail.create(name: "Moscow mule")
+
+
