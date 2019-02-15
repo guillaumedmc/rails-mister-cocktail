c069889fe94192e38f7ed2e675c9c85003611e06
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-02-15 15:04:06 +0100

    fini

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,4 +8,8 @@
 // layout file, like app/views/layouts/application.html.erb
 import 'bootstrap';
 
-console.log('Hello World from Webpacker')
+import 'select2/dist/css/select2.css';
+import { initSelect2 } from '../components/select2';
+
+initSelect2();
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'cocktails', to: 'cocktails#index'
 
+  root to: "cocktails#index"
+
   resources :cocktails do
     resources :doses, only: [ :new, :create ]
   end
